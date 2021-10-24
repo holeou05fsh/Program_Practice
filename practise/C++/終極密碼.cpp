cb0122c080c15f0429f185e4b5719546cd5c1481
holeou05fsh <holeou05fsh@gmail.com>
2021-10-24 11:08:02 +0800

    1.新增終極密碼.cpp內容(可執行)

--- practise/C++/終極密碼.cpp
+++ practise/C++/終極密碼.cpp
@@ -1,2 +1,61 @@
 #include <iostream>
-#include 
+#include <string>
+#include <stdio.h>
+#include <random>
+#include <ctime>
+using namespace std;
+
+
+int main(void)
+{
+    int min = 1;
+    int max = 100;
+    int range_min = min;
+    int range_max = max;
+
+    mt19937 rd(time(NULL));
+    uniform_int_distribution<int> dis(min, max);
+    int pass = dis(rd);
+    //cout << pass << endl;
+
+    int guess = 0;
+
+    do
+    {
+        cout << "請輸入" << range_min << "-"<< range_max << ":" << endl;
+        cin >> guess;
+
+        //輸入錯誤範圍
+        if(guess > range_max+1 || guess < range_min-1)
+        {
+            cout << "輸入錯誤。" << endl;
+            continue;
+        }
+        //比答案大
+        if(guess > pass)
+        {
+            range_max = guess;
+            cout << "安全。" << endl;
+            continue;
+        }
+        //比答案小
+        if(guess < pass)
+        {
+            range_min = guess;
+            cout << "安全。" << endl;
+            continue;
+        }
+        //猜中
+        if(guess == pass)
+        {
+            cout << "中了，遊戲結束";
+            break;
+        }
+
+    } while (guess != pass);
+    system("pause");
+
+
+
+    return 0;
+}
